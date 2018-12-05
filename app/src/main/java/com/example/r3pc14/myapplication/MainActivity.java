de743d5228b441c9c3658d93ebdda1aac01bccf2
ekanrd <45616457+ekanrd@users.noreply.github.com>
2018-12-05 13:52:00 +0700

    commit

--- app/src/main/java/com/example/r3pc14/myapplication/MainActivity.java
+++ app/src/main/java/com/example/r3pc14/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView;
+    TextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
